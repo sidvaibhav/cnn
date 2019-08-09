f68139ca499bc79e451fde62d3c84ffe66603232
Siddanth Vaibhav <37875263+sidvaibhav@users.noreply.github.com>
2019-08-09 23:45:33 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -52,12 +52,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('C:/Users/ankit/OneDrive/Desktop/Convolutional_Neural_Networks/dataset/training_set',
+training_set = train_datagen.flow_from_directory('C:/Users/siddanth/OneDrive/Desktop/Convolutional_Neural_Networks/dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('C:/Users/ankit/OneDrive/Desktop/Convolutional_Neural_Networks/dataset/test_set',
+test_set = test_datagen.flow_from_directory('C:/Users/siddanth/OneDrive/Desktop/Convolutional_Neural_Networks/dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
